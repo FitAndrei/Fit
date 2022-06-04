7ea751e7227622ab221fb04b074384da09e6bdaa
FitAndrei <106863453+FitAndrei@users.noreply.github.com>
2022-06-04 11:28:49 +0300

    Add files via upload

---
+++ Ceva/ok.js
@@ -0,0 +1,4 @@
+function calculate() {
+    document.getElementById("totalId").value = Number(document.getElementById("AId").innerHTML) + 
+      Number(document.getElementById("bId").innerHTML);
+  }
